8784ba9b44b2a3bf2d133d05184d451b8848599c
Meitar Shimoni <141142200+MeitarShimoni@users.noreply.github.com>
2025-12-03 20:50:10 +0200

    Update and rename uart_fsm_1.sv to uart_tx_phy_fsm.sv

--- design/uart_tx_phy_fsm.sv
+++ design/uart_tx_phy_fsm.sv
@@ -1,4 +1,4 @@
-module uart_fsm_1(
+module uart_tx_phy_fsm(
     input system_clock,
     input clock_enable,
     input rst_n,
@@ -9,25 +9,16 @@ module uart_fsm_1(
     output reg Tx
     );
     
-    localparam int CNT_50MS = 2881/2;
-    localparam int CNT_100MS = 5761/2;
-    localparam int CNT_200MS = 11521/2;
-    
-
-//localparam IDLE = 3'b000;
-//localparam START = 3'b001;
-//localparam DATA = 3'b010;
-//localparam STOP = 3'b100;
+localparam int CNT_50MS = 2881/2;
+localparam int CNT_100MS = 5761/2;
+localparam int CNT_200MS = 11521/2;
 
 typedef enum logic [2:0] {IDLE,START,DATA,STOP,WAIT} state_e;
 
 state_e current_state, next_state;
-//reg [3:0] current_state, next_state;
 reg [3:0] data_counter;
 reg [7:0] data_reg; 
 
-// delay:
-// reg start_count;
 reg [14:0] delay_val;
 reg [14:0] count_delay;
 
@@ -57,13 +48,12 @@ always @(posedge system_clock or negedge rst_n) begin
         tx_busy  <= 0;
         data_reg <= 0;
         end else if(clock_enable) begin
-//    $display("State: %s", current_state);
-    case (next_state) // ? MAYBE TO USE UNIQUE CASE SINCE IT'S ONE HOT ?
+    case (next_state) 
     IDLE: 
         begin
             Tx <= 1;
             tx_busy <= 0;
-            // start_count <= 0;
+            
         end
     START: 
         begin
@@ -91,9 +81,10 @@ always @(posedge system_clock or negedge rst_n) begin
     endcase
     end
 end
+
 // -------------------------------------------------------------------
 always @(*) begin
-    case(current_state)     // ? MAYBE TO USE UNIQUE CASE SINCE IT'S ONE HOT ?    
+    case(current_state)        
         IDLE: next_state = (center_push == 1) ? START : IDLE;
         START: next_state = (current_state == START) ? DATA : START;
         DATA: next_state = (data_counter == 8) ? STOP : DATA;
@@ -102,20 +93,5 @@ always @(*) begin
         default: next_state = IDLE;
     endcase
 end
-
-// always @(posedge system_clock or negedge rst_n) begin
-//     if(!rst_n) begin
-//         delay_counter <= 0;
-//     end else if(clock_enable) begin
-//         if(start_count) begin
-//             if(delay_counter == delay) begin
-//                 delay_counter <= 0;
-//             end else delay_counter <= delay_counter + 1;
-//         end else
-//         delay_counter <= 0;
-//     end
-
-// end
-
 
 endmodule
